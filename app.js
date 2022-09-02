c67102fb0cf7ea4655c5ef030456106a085cd941
martindellavecchia <dellavec@gmail.com>
2022-09-02 00:09:28 -0300

    Armado de estructura de carpetas y servidor web

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express"); 
+const path = require("path");              
+const app = express();                   
+app.use(express.static(path.resolve(__dirname, "./public")));  //sirviendo contenido estatico
+const puerto = 3000; //Webserver Port
+app.listen(3000, () => {
+    console.log(`Servidor Web corriendo en el puerto ${puerto}`);
+})
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
