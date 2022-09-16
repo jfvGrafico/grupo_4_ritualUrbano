a9c1bd05e70fe106d968c6a39cb8e5565a05c62e
martindellavecchia <dellavec@gmail.com>
2022-09-15 23:47:03 -0300

    se agrego metodo POST a registro y login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");              
 const app = express();                   
 app.use(express.static(path.resolve(__dirname, "./public")));  //sirviendo contenido estatico
+app.use(express.urlencoded({ extended: false })); //
 const puerto = 3000; //Webserver Port
 app.listen(3000, () => {
     console.log(`Servidor Web corriendo en el puerto ${puerto}`);
@@ -20,4 +21,16 @@ app.get("/carrito", (req, res) =>{
 })
 app.get("/detalle", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/detalle.html"))
+})
+
+// POST
+
+app.post("/registro", (req, res) =>{
+    res.redirect("/")
+    console.log(req.body)
+})
+
+app.post("/login", (req, res) =>{
+    res.redirect("/")
+    console.log(req.body)
 })
