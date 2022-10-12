439931974b5aa7fb0ffef104c5dd6479a208f528
martindellavecchia <dellavec@gmail.com>
2022-10-12 17:09:52 -0300

    se corrigio controller busqueda

--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -30,8 +30,8 @@ router.get ("/carrito", productoController.carrito)
 router.get("/crear" , productoController.crear )
 router.post("/" , upload.any(), productoController.nuevoProd)
 router.get("/:prodID/editar" , productoController.editar)
-router.delete("/:prodID", productoController.eliminar)
 router.get("/resultado",  productoController.resultado)
+router.delete("/:prodID", productoController.eliminar)
 router.get ('/:prodID', productoController.productoDetalle) // Esta linea debe ser la ultima ruta.
 
 
