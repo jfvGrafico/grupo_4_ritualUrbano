5eb95cd1f378441994c50a34e7d71cfad68d305f
martindellavecchia <dellavec@gmail.com>
2022-10-20 13:08:10 -0300

    se incorporo validacion al form de registro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const path = require ("path")
 const fs = require("fs")
+const {validationResult} = require ("express-validator")
+const { traceDeprecation } = require("process")
 let users = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/users.json") , "utf-8"))
 
 const userController = {
@@ -15,7 +17,14 @@ const userController = {
     },
 
     registroPost: (req, res) =>{
-        res.send(req.body)
+        let errors = validationResult(req)
+        if(errors.isEmpty()){
+            res.send(req.body)
+        } else {
+            res.render("users/registro",  {title : "registro", mensajeDeError : errors.mapped()})
+        }
+        
+
     }
 
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,13 +1,21 @@
 const express = require ("express");
+const {body} = require("express-validator")
 const userController = require("../controllers/userController");
 const router = express.Router();
 
+//validacion de registro de usuario
+validateRegistro = [
+    body("nombre").notEmpty().withMessage("El nombre es un campo obligatorio"),
+    body("email").isEmail().withMessage("Por favor ingrese un email valido"),
+    body("password").notEmpty().withMessage("Recuerde introducir la contraseña"),
+    body("rpassword").notEmpty().withMessage("Recuerde repetir la contraseña")
+]
 
 //rutas de usuario.
 router.get("/login", userController.login )
 router.get("/registro", userController.registro)
 router.post("/login", userController.loginPost )
-router.post("/registro", userController.registroPost)
+router.post("/registro",validateRegistro, userController.registroPost)
 
 module.exports = router;
 
