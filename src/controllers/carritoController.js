1d545e6ccc3bdc908140cfec2e10f67b07a9da08
martindellavecchia <dellavec@gmail.com>
2022-10-25 13:54:30 -0300

    se separo el carrito en su propia ruta y controlador

---
+++ src/controllers/carritoController.js
@@ -0,0 +1,33 @@
+const path = require ("path")
+const fs = require("fs")
+const productos = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json") , "utf-8"))
+const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
+
+const carritoController = {
+    carrito: (req, res)=>{
+        const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
+          res.render("products/carrito", { title: "Carrito", carrito });           
+      },
+    guardarCarrito: (req, res) => {
+     
+        let productSelect = productos.find((producto) => req.params.id == producto.id);
+        carrito.push(productSelect)
+        let arrayAGuardar = JSON.stringify(carrito, null, " ");
+        let pathToFile = path.join(__dirname, "../data/carrito.json");
+        fs.writeFileSync(pathToFile, arrayAGuardar);
+        req.session.carritoSession = carrito
+        res.redirect("/producto/carrito")
+        
+    
+      },
+      carritoDelete : (req, res) => {
+     
+        let aBorrar = carrito.filter(item => item.id != req.params.carritoID)
+        let aGuardar = JSON.stringify(aBorrar, null, " ");
+        let pathToFile = path.join(__dirname, "../data/carrito.json");
+        fs.writeFileSync(pathToFile, aGuardar);
+        res.redirect("/producto/carrito")
+      }
+}
+
+module.exports = carritoController
