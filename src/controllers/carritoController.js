758670ff0ce4361d947ed41b76f217f02f60d3de
martindellavecchia <dellavec@gmail.com>
2022-10-25 18:01:11 -0300

    se corrigio middleware de carrito

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,17 +1,18 @@
 const path = require ("path")
 const fs = require("fs")
 const productos = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/products.json") , "utf-8"))
-const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
 
 
 
 const carritoController = {
     carrito: (req, res)=>{
+        const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
           res.render("products/carrito", { title: "Carrito", carrito });           
       },
 
 
     guardarCarrito: (req, res) => {
+        const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
         let productSelect = productos.find((producto) => req.params.id == producto.id);
         carrito.push(productSelect)
         let arrayAGuardar = JSON.stringify(carrito, null, " ");
@@ -23,14 +24,15 @@ const carritoController = {
     
       },
       carritoDelete : (req, res) => {
-        if(req.session.carritoSession != undefined){
+        const carrito = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/carrito.json") , "utf-8"))
+
             let aBorrar = carrito.filter(item => item.id != req.params.carritoID)
             let aGuardar = JSON.stringify(aBorrar, null, " ");
             let pathToFile = path.join(__dirname, "../data/carrito.json");
             fs.writeFileSync(pathToFile, aGuardar);
-            req.session.destroy();
+            req.session.destroy()
             res.redirect("/producto/carrito")
-      }
+      
 }}
 
 module.exports = carritoController
