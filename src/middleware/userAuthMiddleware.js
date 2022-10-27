5ddf31b8e07f5bc764b908193c7389766ccdde38
martindellavecchia <dellavec@gmail.com>
2022-10-27 12:59:45 -0300

    se agrego nombre de usuario en header

--- src/middleware/userAuthMiddleware.js
+++ src/middleware/userAuthMiddleware.js
@@ -1,8 +1,9 @@
 const userAuthMiddleware = (req, res, next) => {
         res.locals.logeo = false  
-    if(req.session.usuarioLogeado != undefined)
+    if(req.session.usuarioLogeado)
     {
-        res.locals.logueo = true  
+        res.locals.logueo = true 
+        res.locals.userID = req.session.usuarioLogeado.first_name
         res.locals.userType = req.session.usuarioLogeado.category
     }
     next()
