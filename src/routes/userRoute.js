e0a64e1311b9a9adb7838743430b2818ae22bbab
Dwavid96 <dwavid96@gmail.com>
2022-10-30 21:52:46 -0300

    cambios de avatar

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -9,7 +9,7 @@ const multer = require("multer")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/img/')
+      cb(null, 'public/img/users')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
