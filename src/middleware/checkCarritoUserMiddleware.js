d041e710008ffe96d1658d07a17f1bfd18956b23
Dwavid96 <dwavid96@gmail.com>
2022-11-08 20:47:43 -0300

    rutas de verificación

---
+++ src/middleware/checkCarritoUserMiddleware.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const checkCarritoUserMiddleware = (req, res, next) =>{
+    
+    next()
+}
+
+module.exports = checkCarritoUserMiddleware
--- src/routes/carritoRoute.js
+++ src/routes/carritoRoute.js
@@ -1,13 +1,13 @@
 const express = require ("express");
 const router = express.Router();
 const carritoController = require("../controllers/carritoController");
-
+const checkCarritoUserMiddleware = require("../middleware/checkCarritoUserMiddleware")
 
 //rutas de carrito
 
 router.get ("/", carritoController.carrito)
 router.delete ("/:carritoID", carritoController.carritoDelete)
 router.post("/:id", carritoController.guardarCarrito)
-router.get("/compraFin", carritoController.compraFin);
+router.get("/compraFin",checkCarritoUserMiddleware, carritoController.compraFin);
 
 module.exports = router;
