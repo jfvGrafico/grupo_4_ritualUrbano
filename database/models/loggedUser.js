54fa969361c631bad10fd3e1ec19de777864aac2
martindellavecchia <dellavec@gmail.com>
2022-11-16 08:59:29 -0300

    se incorporo el modelo loggedUser

---
+++ database/models/loggedUser.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "loggedUser"
+    const cols = {
+        id : {
+            type : dataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        first_name : dataTypes.STRING,
+        last_name : dataTypes.STRING,
+        email:  dataTypes.STRING,
+        category:  dataTypes.STRING,
+        image:  dataTypes.STRING,
+        token:  dataTypes.STRING,
+    }
+
+    const config = {
+        tableName : "loggedUser",
+        timestamps: false
+    }
+
+    const loggedUser =  sequelize.define(alias, cols, config)
+    return loggedUser
+}
