a7a016b55802f25f3b5defbc7d09e7da54f183e9
martindellavecchia <dellavec@gmail.com>
2022-11-16 08:52:53 -0300

    se incorporo el modelo User

---
+++ database/models/User.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "Users"
+    const cols = {
+        id : {
+            type : dataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        first_name : dataTypes.STRING,
+        last_name : dataTypes.STRING,
+        password:  dataTypes.STRING,
+        email:  dataTypes.STRING,
+        category:  dataTypes.STRING,
+        image:  dataTypes.STRING,
+    }
+
+    const config = {
+        tableName : "users",
+        timestamps: false
+    }
+
+    const User =  sequelize.define(alias, cols, config)
+    return User
+}
