203054c8755dede62c84af6bcab4ab0fb0dee846
martindellavecchia <dellavec@gmail.com>
2022-11-17 22:22:25 -0300

    se agrego el modelo Producto

---
+++ database/models/Product.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "Products"
+    const cols = {
+        id : {
+            type : dataTypes.INTEGER,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        nombre : dataTypes.STRING,
+        descripcion : dataTypes.STRING,
+        categoria:  dataTypes.STRING,
+        peso:  dataTypes.INTEGER,
+        imagen:  dataTypes.STRING,
+        precio:  dataTypes.INTEGER,
+    }
+
+    const config = {
+        tableName : "products",
+        timestamps: false
+    }
+
+    const Product =  sequelize.define(alias, cols, config)
+    return Product
+}
