71cb83f3766e0ec3cb41296cb1c8a0feed1cbc94
epere18 <epere18@hotmail.com>
2023-01-23 14:39:54 -0300

    Actualizando Api Productos

--- src/controllers/api/productoApiController.js
+++ src/controllers/api/productoApiController.js
@@ -1,72 +1,125 @@
-const path = require('path');
-const db = require('../../database/models');
+const path = require("path");
+const db = require("../../database/models");
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
-const moment = require('moment');
-
+const moment = require("moment");
 
 const productoApiController = {
+  sqlSearch:
+    'select categoryproducts.nombre, count(*) as "total" from products inner join categoryproducts on idCategoria = categoryproducts.id group by categoryproducts.id',
 
-    sqlSearch:'select categoryproducts.nombre, count(*) as "total" from products inner join categoryproducts on idCategoria = categoryproducts.id group by categoryproducts.id',
-    
-    list: async (req, res) => {
-        
-        try {
-            let productos = await db.Product.findAll({ include: ["CategoryProduct"] });
-            let categorias = await db.sequelize.query(productoApiController.sqlSearch, {type: sequelize.QueryTypes.SELECT})  
+  list: async (req, res) => {
+    try {
+    //   let categorias = await db.sequelize.query(
+    //     productoApiController.sqlSearch,
+    //     { type: sequelize.QueryTypes.SELECT }
+    //   );
+      let productos = await db.Product.findAll({
+        include: ["CategoryProduct"],
+        attributes: { exclude: ["idCategoria"] }
+      });
 
-            res.status(200).json(
-                {
-                    meta: {
-                        status: 200,
-                        count: productos.length,
-                        countByCategory: categorias,
-                        url: 'api/products'
-                    },
-                    data: {
-                        products:productos
-                    }
+      let countCafe = 0;
+      let countCafetera = 0;
+      let countMerchandising = 0;
+      let coutnOtrosProductos = 0;
 
-                }
-            )
-        } catch (error) {
-            console.log(error)
+      /* Contador de productos por categoria */
+
+      for (let i = 0; i < productos.length; i++) {
+        if (productos[i].CategoryProduct.nombre == "cafe") {
+            countCafe += 1;
+        }else if(productos[i].CategoryProduct.nombre == "cafetera") {
+            countCafetera += 1;
+        }else if(productos[i].CategoryProduct.nombre == "merchandising") {
+            countMerchandising += 1;
+        }else if(productos[i].CategoryProduct.nombre == "otrosProductos") {
+            coutnOtrosProductos += 1;
         }
-    },
+      }
+      /* Imprime campo detail en producto con url api */
 
-    categorias: (req, res) => {
-        db.CategoryProduct.findAll()
-        .then(categorias => {
-            let respuesta = {
-                meta: {
-                    status : 200,
-                    count: categorias.length,
-                    url: "api/products",                    
-                },
-                categorias: categorias
-            }
-                res.json(respuesta);
-            })
-    },
+      for (let i = 0; i < productos.length; i++) {
+        productos[i].setDataValue(
+          "detail",
+          `http://localhost:3000/api/products/${productos[i].id}`
+        );
+      }
 
-    detalleProducto: (req, res) => {
-        db.Product.findByPk(req.params.id, {include: ["CategoryProduct"],})
-                .then(producto => {
-            let respuesta = {
-                meta: {
-                    status : 200,
-                    url: "api/products/:id",
-                    producto: producto,
-                   
-                },
-            }
-            
-                res.json(respuesta);
-            })
+      /* Imprime url de la foto para consumir */
 
+      for (let i = 0; i < productos.length; i++) {
+        productos[i].setDataValue(
+          "urlImgen",
+          `http://localhost:3000/${productos[i].imagen}`
+        );
+      }
 
+      let response = {        
+        status: 200,
+        url: "http://localhost:3000/api/products",
+        count: productos.length,
+        countByCategory: [
+          {
+            cafe: countCafe,
+            cafetera: countCafetera,
+            merchandising: countMerchandising,
+            otrosProductos: coutnOtrosProductos,
+          },
+        ],       
+        data: productos,
+
+      };
+
+      res.status(200).json(response);
+
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
 
+  categorias: (req, res) => {
+    db.CategoryProduct.findAll().then((categorias) => {
+      let respuesta = {
+        meta: {
+          status: 200,
+          listaProductos: "http://localhost:3000/api/products",
+          count: categorias.length,          
+        },
+        categorias: categorias,
+      };
+      res.json(respuesta);
+    });
+  },
+
+  detalleProducto: (req, res) => {
+    db.Product.findByPk(req.params.id, { include: ["CategoryProduct"] }).then(
+      (productos) => {
+        // for (let i = 0; i < productos.length; i++) {
+        //     productos[i].setDataValue(
+        //       "urlImagen",
+        //       `http://localhost:3000${productos[i].imagen}`
+        //     );
+        // };
+        let respuesta = {
+          
+            status: 200,
+            listaProductos: "http://localhost:3000/api/products",
+            detalleProducto: {
+            id: productos.id,
+            nombre: productos.nombre,
+            descripcion: productos.descripcion,
+            peso: productos.peso,
+            imagenProducto: `http://localhost:3000${productos.imagen}`,          
+            precio: productos.precio,
+            categoria: productos.CategoryProduct.nombre,            
+          },
+        };
+
+        res.json(respuesta);
+      }
+    );
+  },
+};
 
 module.exports = productoApiController;
